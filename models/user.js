fcc0c14a8ce17ad2600f55190e03d2653d48ee36
Glitch (bouncy-top) <none>
2017-12-13 17:53:40 +0000

    🍞🔔 Updated with Glitch

--- models/user.js
+++ models/user.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
-
 const UserSchema = new mongoose.Schema({
-  
-  twitter: {
-    id: {
-      type: Number,
-      unique: true
-    },
-    token: String,
-    username: String
-  },
-  
-  firstname: String,
-  lastname: String,
-  city: String,
-  state: String
-  
+	twitter: {
+		id: {
+			type: Number,
+			unique: true
+		},
+		token: String,
+		username: String
+	},
+	firstname: String,
+	lastname: String,
+	city: String,
+	state: String
 });
-
 module.exports = mongoose.model('User', UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-
 router.get('/auth/twitter', passport.authenticate('twitter'));
-router.get('/auth/twitter/callback',
-  passport.authenticate('twitter', {
-    failureRedirect: '/error',
-    successRedirect: '/'
-  })
-);
-
+router.get('/auth/twitter/callback', passport.authenticate('twitter', {
+	failureRedirect: '/error',
+	successRedirect: '/'
+}));
 router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
-
 module.exports = router;
